fe060b1570966d9aa5c7945c9909d85d5c0a8bf8
Bastian Rihm <brihm@intevation.de>
2026-03-24 17:20:33 +0100

    Add organization triggers (#432)

--- dev/src/validate.py
+++ dev/src/validate.py
@@ -222,14 +222,14 @@ class Checker:
         if field.get("calculated"):
             return
 
-        if field_name := field.get("sequence_scope", ""):
+        if scope_field_name := field.get("sequence_scope", ""):
             if type != "number":
                 self.errors.append(
                     f"Sequences can only be generated for number fields. {collectionfield} is {type}."
                 )
-            if field_name not in self.models[collection]:
+            if scope_field_name not in self.models[collection]:
                 self.errors.append(
-                    f"{field_name} can not be used as a source of sequence scope since it is not part of {collection}."
+                    f"{scope_field_name} can not be used as a source of sequence scope since it is not part of {collection}."
                 )
 
         valid_attributes = list(OPTIONAL_ATTRIBUTES) + required_attributes
@@ -284,6 +284,10 @@ class Checker:
             if nested and type in ("relation", "relation-list"):
                 valid_attributes.append("enum")
             valid_attributes.extend(("reference", "deferred", "sql"))
+            if "default" in field and field_name == "organization_id":
+                # added as a workaround to allow defaulting to the ONE_ORGANIZATION
+                print(f"Default in {collection}/{field_name} temporarily allowed.")
+                valid_attributes.append("default")
 
         for attr in field.keys():
             if attr not in valid_attributes:
